aee2be455a39336b0fc868a1f3fc406d528639e0
aggarwal-shubham02 <44107268+aggarwal-shubham02@users.noreply.github.com>
2021-09-07 18:08:16 +0530

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World, all done work!!");
+        System.out.println("Hello World, all done work checking new changes !!");
     }
 }
